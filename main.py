d37db23573d9fc4c5c77c8768a9d5e085ebf7581
Samay <samaypowade1@gmail.com>
2025-08-22 16:00:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from pydantic import BaseModel
-from pymongo import MongoClient
+from fastapi import FastAPI
 
 app = FastAPI()
 
@@ -8,3 +7,12 @@ app = FastAPI()
 @app.get("/")
 def show():
     return "sarted...."
+
+lists = [1,2,3,4,5]
+
+@app.get("/show")
+def my():
+    return lists
+
+
+
